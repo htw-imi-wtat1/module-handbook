37e6f2531e7f3ca902614f795d941061e354419d
B Kleinen <kleinen@infrastructure.de>
2020-05-02 18:24:08 +0200

    seed script opening resulting courses page after completion

---
+++ mongo/seed/seedDB.js
@@ -0,0 +1,29 @@
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/modulehandbook_db",
+    mongoose = require("mongoose"),
+    Course = require("../../models/course");
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const courseData = require("./imi-b-courses");
+
+function openCourseOverview() {
+    if (/localhost/.test(mongoURI)) {
+        const open = require('open');
+        open('http://localhost:3002/courses');
+    }
+}
+
+Course.deleteMany({})
+    .then(() => {
+        console.log("all courses deleted");
+    })
+    .then(() => {
+        return Course.create(courseData);
+    })
+    .catch(error => console.log(error.message))
+    .then(createdCourses => {
+        console.log(createdCourses.length + " courses created")
+        mongoose.connection.close();
+        openCourseOverview();
+    });
+
