8837c0b0b7f89386262a7a1b1944ecd7a33609e0
B Kleinen <kleinen@infrastructure.de>
2020-05-03 16:35:37 +0200

    added test that actually smoke-tests the rendered page

--- test/coursesControllerIndexSpec.js
+++ test/coursesControllerIndexSpec.js
@@ -5,36 +5,45 @@ chai.use(chaiHTTP)
 const { expect } = chai
 const app = require('../app')
 const Course = require('../models/course')
-const testCourseData = {
-  code: 'B16',
-  name: '2. Fremdsprache',
-  mission: 'learn language',
-  ects: 4,
-  examination: 'Exam',
-  objectives: 'learn language',
-  contents: 'verbs, nouns and adjectives',
-  prerequisites: 'basic course in same language',
-  literature: 'text book',
-  methods: 'WP Ü',
-  skills_knowledge_understanding: 'skills 1',
-  skills_intellectual: 'skills 2',
-  skills_practical: 'skills 3',
-  skills_general: 'skills 4'
-}
+const courseData = require('../mongo/seed/imi-b-courses')
+const threeCourses = [courseData[3], courseData[15], courseData[23]]
+
 beforeEach(function (done) {
   Course.deleteMany({})
     .then(() => {
+      console.log('all courses deleted')
+    })
+    .then(() => {
+      return Course.create(threeCourses)
+      // to see tests fail, use this:
+      // return Course.create([{code: "dummy"}])
+    })
+    .catch(error => done(error.message))
+    .then(createdCourses => {
+      console.log(createdCourses.length + ' courses created')
       done()
     })
 })
 describe('coursesController', function () {
   describe('course list', function () {
-    it('show all courses in db', function (done) {
+    it('show ok on /courses', function (done) {
       chai.request(app)
         .get('/courses')
         .end((errors, res) => {
           expect(res).to.have.status(200)
           expect(errors).to.be.equal(null)
+          done()
+        })
+    })
+    it('show all courses in db', function (done) {
+      chai.request(app)
+        .get('/courses')
+        .end((errors, res) => {
+          const body = res.text
+          for (const course of threeCourses) {
+            expect(body).to.include(course.code)
+            expect(body).to.include(course.name)
+          }
           done()
         })
     })
